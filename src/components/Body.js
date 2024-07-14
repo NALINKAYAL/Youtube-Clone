1a166ca4d69082a74a4589450b23bac62305e870
NALINKAYAL <nvkayal_b21@el.vjti.ac.in>
2024-07-15 00:54:00 +0530

    second commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import Sidebar from './Sidebar';
-import Feed from './Feed';
-import {Outlet} from 'react-router-dom';
- 
+import React from "react";
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
     <div className="flex mt-16">
-        <Sidebar />
-        <Outlet/>
-      </div>
-  )
-}
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+};
 
-export default Body
+export default Body;
